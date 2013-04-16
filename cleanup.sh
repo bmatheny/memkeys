e3b2a461a327df98ec51f2c3b02af0586b536cc3
Blake Matheny <bmatheny@mobocracy.net>
2013-04-16 15:25:07 -0400

    Still working on builds

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+
+rm -rf Makefile.in \
+        aclocal.m4 autom4te.cache/ config.guess config.sub \
+        configure depcomp install-sh ltmain.sh mconfig.h.in \
+        missing src/.deps src/Makefile src/Makefile.in

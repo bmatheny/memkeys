fd867cec8bc786e777919b894dcfc6ce59ccd027
Blake Matheny <bmatheny@mobocracy.net>
2013-04-16 19:05:29 -0400

    Ditching unused variable

--- src/cli.h
+++ src/cli.h
@@ -6,7 +6,6 @@
 class Cli
 {
 public:
-    const std::string Argp = "d:i:p:r:hv";
     static void parse(int argc, char ** argv, Config * cfg);
     static std::string help(const char * progname);
 };

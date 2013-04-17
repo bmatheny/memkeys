4459f2e9b59e2a4022a415dc4fdcdc974003dd09
Blake Matheny <bmatheny@mobocracy.net>
2013-04-17 11:33:40 -0400

    Help text, refs #1

--- src/cli.cpp
+++ src/cli.cpp
@@ -7,6 +7,17 @@
 #include "config.h"
 #include "options.h"
 
+#ifdef HAVE_CONFIG_H
+#include "mconfig.h"
+#endif
+
+#ifndef PACKAGE_STRING
+#define PACKAGE_STRING "mctop (unknown version)"
+#endif
+#ifndef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "bmatheny@mobocracy.net"
+#endif
+
 static const struct option longopts[] = {
     {"discard", required_argument, 0, 'd'},
     {"interface", required_argument, 0, 'i'},
@@ -14,14 +25,26 @@ static const struct option longopts[] = {
     {"refresh", required_argument, 0, 'r'},
     {"help", no_argument, 0, 'h'},
     {"verbose", no_argument, 0, 'v'},
+    {"version", no_argument, 0, 'V'},
 };
-static const char * argp = "d:i:p:r:hv";
+static const char * argp = "d:i:p:r:hvV";
 
-std::string Cli::help(const char * progname)
+using namespace std;
+
+string Cli::help(const char * progname)
 {
-    std::ostringstream txt;
-    std::string pname = progname;
-    txt << "Usage: " << pname << std::endl;
+    ostringstream txt;
+    string pname = progname;
+    string tab = "    ";
+    txt << "Usage: " << pname << " -i NIC [options]" << endl;
+    txt << mkHelpDoc(longopts[0], "Discard keys where req/s rate is below THRESH", "THRESH");
+    txt << mkHelpDoc(longopts[1], "Network interface to capture traffic on (required)", "NIC");
+    txt << mkHelpDoc(longopts[2], "Network port to capture memcache traffic on (default 11211)", "PORT");
+    txt << mkHelpDoc(longopts[3], "Refresh the stats display every INTERVAL ms (default 500)", "INTERVAL");
+    txt << endl;
+    txt << mkHelpDoc(longopts[4], "This help", "");
+    txt << mkHelpDoc(longopts[5], "Increase verbosity. May be used multiple times.", "");
+    txt << mkHelpDoc(longopts[6], "Show program info and exit.", "");
     return txt.str();
 }
 
@@ -51,20 +74,50 @@ void Cli::parse(int argc, char ** argv, Config * cfg)
                 cfg->setRefreshInterval(::atoi(optarg));
                 break;
             case 'h':
-                std::cout << Cli::help(progname) << std::endl;
+                cout << Cli::help(progname) << endl;
                 exit(EXIT_SUCCESS);
             case 'v':
                 cfg->increaseVerbosity();
                 break;
+            case 'V':
+                cout << "Package: " << PACKAGE_STRING << endl;
+                cout << "Author : " << PACKAGE_BUGREPORT << endl;
+                cout << "License: Apache 2.0" << endl;
+                exit(EXIT_SUCCESS);
             case '?':
-                std::cerr << "Unknown flag specified" << std::endl;
-                std::cout << Cli::help(progname) << std::endl;
+                cerr << "Unknown flag specified" << endl;
+                cout << Cli::help(progname) << endl;
                 exit(EXIT_FAILURE);
             default:
-                std::cerr << "Unknown flag specified" << std::endl;
-                std::cout << Cli::help(progname) << std::endl;
+                cerr << "Unknown flag specified" << endl;
+                cout << Cli::help(progname) << endl;
                 exit(EXIT_FAILURE);
         }
     }
+}
 
+string Cli::mkHelpLead(const struct option opt, const string &key)
+{
+    ostringstream txt;
+    ssize_t len = 0, pad = 33, alloc = 33;
+    char * os = NULL;
+    txt << "    -" << ((char)opt.val) << ", --" << opt.name;
+    if (opt.has_arg == 1) {
+        txt << "=" << key;
+    } else if (opt.has_arg == 2) {
+        txt << "[=" << key << "]";
+    }
+    len = txt.str().length();
+    alloc += len;
+    os = (char*)malloc(sizeof(char)*alloc);
+    snprintf(os, alloc, "%-32s", txt.str().c_str());
+    return string(os);
+}
+
+string Cli::mkHelpDoc(const struct option opt, const string &desc, const string &key)
+{
+    ostringstream txt;
+    string lead = mkHelpLead(opt, key);
+    txt << lead << desc << endl;
+    return txt.str();
 }
--- src/cli.h
+++ src/cli.h
@@ -8,6 +8,13 @@ class Cli
 public:
     static void parse(int argc, char ** argv, Config * cfg);
     static std::string help(const char * progname);
+
+private:
+    static std::string mkHelpLead(const struct option opt,
+                                  const std::string &key);
+    static std::string mkHelpDoc(const struct option opt,
+                                 const std::string &desc,
+                                 const std::string &key);
 };
 
 #endif

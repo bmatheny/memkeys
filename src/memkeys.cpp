8bacb984923bab2265c114294ecf490d1f1660da
Blake Matheny <bmatheny@mobocracy.net>
2013-04-22 16:23:51 -0400

    Remove old comment

--- src/memkeys.cpp
+++ src/memkeys.cpp
@@ -135,7 +135,6 @@ static void process(u_char *userData, const struct pcap_pkthdr* header,
   // FIXME just make this a global static to avoid the weird casting issues
   static CaptureEngine * ce = (CaptureEngine*)userData;
 
-  // FIXME need to include this
   if (ce->isShutdown()) {
     ce->getLogger()->info("Refusing packet capture, in shutdown");
     return;

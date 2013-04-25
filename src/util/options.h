196fa6bae0514f0d0b144a5ea3db107a96ae71b8
Blake Matheny <bmatheny@mobocracy.net>
2013-04-25 17:02:57 -0400

    Fixing 'warning: comparison between signed and unsigned integer expressions'

--- src/util/options.h
+++ src/util/options.h
@@ -18,9 +18,9 @@
 #define USIZE_BITS(type) ((1L << (8*sizeof(type))) - 1)
 #define REQUIRE_UINT(name, value, type) do { \
     uint64_t usize = USIZE_BITS(type); \
-    if ((type)value <= 0) { \
+    if (value <= 0) { \
         throw std::range_error(name " must be >= 0"); \
-    } else if (value > usize) { \
+    } else if ((uint64_t)value > usize) { \
         std::ostringstream oss; \
         oss << name << " must be <= " << usize; \
         throw std::range_error(oss.str()); \
